25d4862998519ecd5abe1baf7934ab4f2f6302c8
Esti Shay and Stephanie Mayer <esti@>
2017-07-11 09:53:33 -0700

    create initial project

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#word-puzzle").submit(function(event) {
+    var puzzleInput = $("input#puzzleString").val();
+    
+
+
+    event.preventDefault()
+  });
+});
