27611b543acc6f96f4b1f4cd4372f281381b3ab9
Esti Shay and Stephanie Mayer <stephanie@>
2017-07-11 10:24:07 -0700

    add code to convert string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
   $("form#word-puzzle").submit(function(event) {
     var puzzleInput = $("input#puzzleString").val();
-    
+    var puzzleOutput = '';
+
+    var vowels = "aeiouAEIOU";
+
+    for (var index = 0; index < puzzleInput.length; index += 1) {
+
+      var char = puzzleInput[index];
+      if (vowels.search(char) >= 0) {
+        puzzleOutput += '-';
+      } else {
+        puzzleOutput += char;
+      }
+    }
+
+    $(".puzzle-result").append(puzzleOutput);
+    // show output
+    // hide form
+    $(".puzzle-result").show();
 
 
-    event.preventDefault()
+
+    event.preventDefault();
   });
 });
