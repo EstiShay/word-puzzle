d1f4cea33ac0abcc9a3c0d7f2cfc6708b04d1b20
Esti Shay and Stephanie Mayer <esti@>
2017-07-11 10:57:48 -0700

    add division of business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
+//UI logic
 $(document).ready(function() {
   $("form#word-puzzle").submit(function(event) {
-    var puzzleInput = $("input#puzzleString").val();
-    var puzzleOutput = '';
-    var vowels = "aeiouAEIOU";
+     puzzleInput = $("input#puzzleString").val();
+     var puzzleOutput = '';
+     var vowels = "aeiouAEIOU";
+     //Business logic begin
+     for (var index = 0; index < puzzleInput.length; index += 1) {
+       var char = puzzleInput[index];
+       if (vowels.search(char) >= 0) {
+         puzzleOutput += '-';
+       } else {
+         puzzleOutput += char;
+       }
+     }
+     //Business logic end
 
-    for (var index = 0; index < puzzleInput.length; index += 1) {
-
-      var char = puzzleInput[index];
-      if (vowels.search(char) >= 0) {
-        puzzleOutput += '-';
-      } else {
-        puzzleOutput += char;
-      }
-    }
-    
     $(".puzzle-result").append(puzzleOutput);
     $(".puzzle-result").show();
     $("form#word-puzzle").hide();
